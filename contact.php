008029116f10ee27a0560e2e4a14d1fbd87a0e73
Kurt Ebrahim <kurt.ebrahim@gmail.com>
2014-01-18 10:50:01 -0500

    fixed padding issues on all pages

--- contact.php
+++ contact.php
@@ -3,12 +3,12 @@ require "includes/header.php";
 ?>
 
    <div id="contactContent">
-   Matthew E. Greenberg, M.S., L.Ac.<br/>
-   10 Naples Lane<br/>
-   Greenlawn, NY 11740<br/>
-   <br/>
-   Phone: (516) 987-4158<br/>
-   Email: <a href="mailto:matthew.greenberg@yahoo.com?subject=Comment+From+Your+Website">matthew.greenberg@yahoo.com</a><br/>
+     Matthew E. Greenberg, M.S., L.Ac.<br/>
+     10 Naples Lane<br/>
+     Greenlawn, NY 11740<br/>
+     <br/>
+     Phone: (516) 987-4158<br/>
+     Email: <a href="mailto:matthew.greenberg@yahoo.com?subject=Comment+From+Your+Website">matthew.greenberg@yahoo.com</a><br/>
    </div>
    <!-- contactContent -->
 <?php
--- index.php
+++ index.php
@@ -2,20 +2,25 @@
 require "includes/header.php";
 ?>
    <div id="homeContent">
-      <span id="logo"><img src="images/logo.gif" width="153" height="151" alt="Five Elements" /></span>
+    <span id="logo"><img src="images/logo.gif" width="153" height="151" alt="Five Elements" /></span>
       <span id="homeText">Acupuncture is based on the theory of the Five Elements, which are represented
       by this symbol.  The Five Elements (or Five Phases) are Fire, Earth, Metal, Water and Wood. They
       symbolize the natural balanced order of the universe and have countless correlations, both in
       nature and in our bodies.  When we understand and honor the order of the Five Phases, our lives
       will be more balanced and healthy.  Acupuncture and acupressure can help to create and maintain
       this balance.<br /><br/>
-      <a href="http://longisland.news12.com/multimedia/long-island-naturally-acupuncture-1.6468020" target="_blank">As Seen on News 12, Long Island Naturally</a>
+      <a href="http://longisland.news12.com/multimedia/long-island-naturally-acupuncture-1.6468020" target="_blank">
+        As Seen on News 12, Long Island Naturally
+      </a>
       <br /><br/>
       <span id="homeSmallText">
         Matthew E. Greenberg, M.S., L.Ac.<br />
         (516) 987-4158<br />
-        <a href="mailto:matthew.greenberg@yahoo.com">matthew.greenberg@yahoo.com</a><br /><br />Accepts Empire Blue Cross Blue Shield Insurance.</span>
-		</span>
+        <a href="mailto:matthew.greenberg@yahoo.com">matthew.greenberg@yahoo.com</a>
+        <br /><br />
+        Provider for Empire Blue Cross Blue Shield
+      </span>
+    </span>
    </div>
    <!-- homeContent -->
 <?php
